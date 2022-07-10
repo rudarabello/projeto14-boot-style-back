bd0182b4297efc94c78e38bb00ee914c69421d59
van-matos <vanmatos@poli.ufrj.br>
2022-07-10 16:19:49 -0300

    feat: get user's cart items

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -2,11 +2,21 @@ import { db, objectId } from "../databases/mongo.js";
 
 export async function AddToCart(req, res) {
     const {product, image, price } = req.body;
-    const { user } = res.locals; //check this
+    const { user } = res.locals;
 
     try {
         await db.collection("cart").insertOne({ product, image, price, userId: new objectId(user._id)});
         res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function GetCartItems(req, res) {
+    const { user } = res.locals;
+    try {
+        const itemsCart = await db.collection("cart").find({ userId: user._id }).toArray();
+        res.status(200).send(itemsCart);
     } catch (error) {
         res.sendStatus(500);
     }
--- src/routers/cartRouters.js
+++ src/routers/cartRouters.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { AddToCart } from "../controllers/cart.controllers.js";
+import { AddToCart, GetCartItems } from "../controllers/cart.controllers.js";
 import { validateTokenMiddleware } from "../middlerwares/validateTokenMiddleware.js";
 
 const router = Router();
 
 router.post("/cart", validateTokenMiddleware, AddToCart);
+router.get("/cart", validateTokenMiddleware, GetCartItems);
 
 export default router;
